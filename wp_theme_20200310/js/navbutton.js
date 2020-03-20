cfe77450439c5aa1d9693bd1ec5380a081f68827
takuma11248250 <takuma112489@gmail.com>
2020-03-21 00:20:34 +0900

    Delete directory

--- wp_theme_20200310/js/navbutton.js
+++
@@ -1,5 +0,0 @@
-jQuery(function() {
-  jQuery("#navbutton").click(function() {
-      jQuery("#header-nav-wrap").slideToggle();
-  });
-});
--- wp_theme_20200310/sidebar.php
+++
@@ -1,6 +0,0 @@
-<aside id="sidebar" class="sidebar">
-  <div class="sidebar-inner">
-  	<?php dynamic_sidebar( 'side-widget' ); ?>
- 
-  </div><!--end sidebar-inner-->
-</aside>
